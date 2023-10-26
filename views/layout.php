9fd9e99af95512090d8212564b7c0fa57532985b
Livni <llmc0383@gmail.com>
2023-10-26 00:12:03 -0600

    modificaciones del layout

--- views/layout.php
+++ views/layout.php
@@ -17,15 +17,15 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="/ejemplo/">
+            <a class="navbar-brand" href="/soliciudes_e/">
                 <img src="<?= asset('./images/logo.png') ?>" width="35px'" alt="cit" >
-                Solicitudes
+                Solicitudes Electrónicas
             </a>
             <div class="collapse navbar-collapse" id="navbarToggler">
                 
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin: 0;">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/ejemplo/"><i class="bi bi-house-fill me-2"></i>Inicio</a>
+                        <a class="nav-link" aria-current="page" href="/soliciudes_e/"><i class="bi bi-house-fill me-2"></i>Inicio</a>
                     </li>
   
                     <div class="nav-item dropdown " >
@@ -42,7 +42,6 @@
                             </li>
                             <li>
                                 <a class="dropdown-item nav-link text-white " href="/aplicaciones/nueva"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Solicitud de licencia temporal</a>
-
                             </li>
                             <li>
                                 <a class="dropdown-item nav-link text-white " href="/aplicaciones/nueva"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Solicitud para Combos/Vallas/Marimbas </a>
@@ -72,6 +71,7 @@
                             </li> 
                         </ul> 
                     </div> 
+                </div> 
                 
                 <div class="col-lg-1 d-grid mb-lg-0 mb-2">
                     <!-- Ruta relativa desde el archivo donde se incluye menu.php -->
