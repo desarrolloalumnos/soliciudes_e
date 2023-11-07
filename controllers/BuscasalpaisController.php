72f65ffe4c78ccaeaf02856c7db3a544c5642ff2
Livni <llmc0383@gmail.com>
2023-11-07 00:03:59 -0600

    se modifico el buscar de la tabla de la solicitud de salidas del pais

--- controllers/BuscasalpaisController.php
+++ controllers/BuscasalpaisController.php
@@ -17,7 +17,22 @@ class BuscasalpaisController{
 
     public static function buscarApi(){
 
-        $sql = "";
+        $sql = "SELECT  
+        ste_cat,
+        TRIM(per_nom1) || ' ' || TRIM(per_nom2) || ' ' || TRIM(per_ape1) || ' ' || TRIM(per_ape2) nombre,
+        sal_salida,
+        sal_ingreso, 
+        pai_desc_lg,
+        dsal_ciudad,
+        pdf_ruta
+        FROM se_salpais
+        inner join se_dsalpais on dsal_sol_salida = sal_id
+        inner join se_autorizacion on aut_id = sal_autorizacion
+        inner join se_solicitudes on aut_solicitud = sol_id
+        inner join se_solicitante on sol_solicitante=ste_id
+        inner join mper on ste_cat = per_catalogo
+        inner join paises on dsal_pais = pai_codigo
+        inner join se_pdf on pdf_solicitud = sol_id";
 
         try {
             $resultado = Salidapais::fetchArray($sql);
